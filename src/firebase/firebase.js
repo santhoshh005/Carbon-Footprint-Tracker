48283e1d04483860756e9c1cf93a0400dc6b54f9
pranaysahithch <pranaysahith.ch@gmail.com>
2026-06-12 19:03:51 +0530

    Remove sensitive Firebase configuration
    Removed Firebase configuration details from firebase.js for security reasons.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyCVvUWEpdbUaMmVnS3CeWfjFXbjPYqC6rY',
-  authDomain: 'carbon-footprint-tracker-54f5c.firebaseapp.com',
-  projectId: 'carbon-footprint-tracker-54f5c',
-  storageBucket: 'carbon-footprint-tracker-54f5c.firebasestorage.app',
-  messagingSenderId: '69835828189',
-  appId: '1:69835828189:web:988aeebd7f913bd2bf2434'
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
